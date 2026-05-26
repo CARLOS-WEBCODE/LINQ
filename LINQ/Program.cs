f010e0bc347fcf4e6699da62c71172b3b3a33b3a
Antonio Rodrigues <antoniobarbosa@fiotec.fiocruz.br>
2026-05-25 23:57:17 -0300

    feat: Where, Skip e Take, SkipLast e TakeLast, SkipWhile e TakeWhile

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,2 +1,18 @@
 ﻿using Dumpify;
 
+IEnumerable<int> listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//Where
+//listaNumeros.Where(num => num > 5).Dump("Números maiores que 5");
+
+//Skip e Take
+//listaNumeros.Skip(5).Dump("Números maiores que 5");
+//listaNumeros.Take(5).Dump("Números menores ou iguais a 5");
+
+//SkipLast e TakeLast
+//listaNumeros.SkipLast(5).Dump("Números menores ou iguais a 5");
+//listaNumeros.TakeLast(5).Dump("Números maiores que 5");
+
+//SkipWhile e TakeWhile
+//listaNumeros.SkipWhile(x => x < 5).Dump("Números maiores ou iguais a 5");
+//listaNumeros.TakeWhile(x => x < 5).Dump("Números menores que 5");
